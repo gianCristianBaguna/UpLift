54b9c5a9d45653fa8e2e953e8caac23d8f0b91bf
Gian Cristian Baguna <usernamenigian@gmail.com>
2025-07-04 11:33:49 +0800

    Add About page and fix navbar layout

---
+++ src/app/pages/about/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-[#2A61AC]">About Page</h1>
+    </main>
+  );
+}
