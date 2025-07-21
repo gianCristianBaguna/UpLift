1a8c489d27d433a824d1ce958101f0571b11075a
aulsied <justinenicole.deguzman-22@cpu.edu.ph>
2025-07-21 09:40:53 +0800

    [Gian][Page] home page hero section wip

--- src/components/home-sections/heroSection.tsx
+++ src/components/home-sections/heroSection.tsx
@@ -15,13 +15,12 @@ export default function HeroSection() {
   }, []);
 
   return (
-    <section className="relative h-[400px] overflow-hidden p-0 m-0 bg-white  shadow-md z-10 -mb-10">
+    <section className="relative h-[600px] overflow-hidden p-0 m-0 bg-white  shadow-md z-10 -mb-10">
       {images.map((img, index) => (
         <div
           key={index}
-          className={`absolute inset-0 w-full h-full transition-opacity duration-1000 ${
-            current === index ? "opacity-100" : "opacity-0"
-          }`}
+          className={`absolute inset-0 w-full h-full transition-opacity duration-1000 ${current === index ? "opacity-100" : "opacity-0"
+            }`}
         >
           <Image
             src={img}
@@ -35,15 +34,31 @@ export default function HeroSection() {
         </div>
       ))}
 
-      {/* Text content */}
       <div className="relative z-20 max-w-7xl mx-auto text-white text-center pt-45 pb-16">
-        <h1 className="font-[Montserrat] text-5xl md:text-7xl lg:text-8xl font-bold tracking-wider transform -rotate-2">
+        <h1 className="font-[Montserrat] text-5xl md:text-7xl lg:text-8xl font-bold tracking-wider transform ">
           <span>SERVE. </span>
           <span className="text-[#F3954A] font-bold drop-shadow-[2px_4px_0_#2A61AC] animate-fall-bounce">
             SHARE.
           </span>
           <span> SHINE.</span>
         </h1>
+        <div className="mt-10">
+          Welcome to <span className="text-[#F3954A] font-bold drop-shadow-[1px_2px_0_#2A61AC] animate-fall-bounce">Uplift Foundation International!</span>  We are driven by the <span className="text-[#F3954A]">passion to empower</span> and <span className="text-[#F3954A]">uplift marginalized communities</span> , inspired by <span className="text-[#F3954A]">personal journeys</span>  and <span className="text-[#F3954A]">commitment to service.</span>
+        </div >
+        <div className="mt-10 flex flex-col lg:flex-row justify-center items-center gap-4 lg:gap-6">
+          <a
+            href="/pages/donation"
+            className="drop-shadow-[2px_4px_0_#2A61AC] bg-[#F3954A] text-white text-lg font-semibold px-6 py-2 rounded-full shadow-lg hover:bg-[#e07f2f] transition-transform transform hover:scale-105 duration-300"
+          >
+            Donate Now
+          </a>
+          <a
+            href="/pages/about"
+            className="bg-white text-black text-lg font-semibold px-6 py-2 rounded-full shadow-lg hover:bg-[#e07f2f] transition-transform transform hover:scale-105 duration-300"
+          >
+            Learn More
+          </a>
+        </div>
       </div>
     </section>
   );
